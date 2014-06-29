8392978bec3fc6f429809c7a407a712cff74ca89
futoase <futoase@gmail.com>
2014-06-29 23:35:52 +0900

    Modified: Support params of wallet.

--- lib/freee/wallet.rb
+++ lib/freee/wallet.rb
@@ -4,8 +4,9 @@ module Freee
       #client.post('/api/1/wallet_txns')
     end
 
-    def self.list(company_id)
-      Freee.client.get("/api/1/wallet_txns?company_id=#{company_id.to_i}")
+    def self.list(company_id, **kwargs)
+      params = Freee.encode_params(kwargs)
+      Freee.client.get("/api/1/wallet_txns?company_id=#{company_id.to_i}#{params}")
     end
   end
 end
